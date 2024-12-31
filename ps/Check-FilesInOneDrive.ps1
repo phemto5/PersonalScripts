f671daaa160c9677a6a6b06776e56f6e5496b485
phemto5 <nicholasaa@gmail.com>
2024-12-31 15:23:18 -0800

    Updated to work more fficently on remote librarys

--- ps/Check-FilesInOneDrive.ps1
+++ ps/Check-FilesInOneDrive.ps1
@@ -1,27 +1,35 @@
 param(
-    $source = "G:\My Drive\Movies"
+  $source = "~/akfmgd/media/all/",
+  $vault = "~/akfostermommyOneDrive/"
 )
 
-$vault = $Env:OneDrive + "/Pictures"
 $NotInOneDrive = "NotInOneDrive.md"
 if (!(Test-Path $NotInOneDrive)) {
-    New-item -ItemType File $NotInOneDrive
+  New-item -ItemType File $NotInOneDrive
 }
 function Check-MatchFileInOtherLocation {
-    param(
-        $from, #Source
-        $to, #Vaule
-        $log
-    )
-    $files = Get-ChildItem $from -recurse
-    foreach ($file in $files) {
-        $results = Get-ChildItem $to -Recurse -Filter $file
-        if (!($results)) {
-            Out-File -InputObject $file -FilePath $log -Append
-        }else{
-            Write-Host Foud: $results[0]
-        }
+  param(
+    $from, #Source
+    $to, #Vaule
+    $log
+  )
+  Write-host "Finding All Files in $from" -foregroundcolor green
+  $fromfiles = Get-ChildItem $from -recurse -name
+  Write-host "Found $($fromfiles.length) Files" -foregroundcolor yellow
+
+  Write-host "Finding All Files in $to" -foregroundcolor green
+  $tofiles = Get-ChildItem $to -recurse -name
+  Write-host "Found $($tofiles.length) Files" -foregroundcolor yellow
+
+  foreach ($file in $fromfiles) {
+    write-host $file -nonewline -foregroundcolor blue
+    if (!($tofiles -contains $file)) {
+      Out-File -InputObject $file -FilePath $log -Append
     }
+    else {
+      Write-Host Foud: $results[0] -ForegroundColor DarkGray -NoNewline
+    }
+  }
 }
 
 Check-MatchFileInOtherLocation $source $vault $NotInOneDrive
