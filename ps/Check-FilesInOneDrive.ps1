c4672108ebb2380d4ad2e903d3d2b84aaeebc61e
phemto5 <nicholasaa@gmail.com>
2025-01-22 09:15:56 -0800

    Small updates for QOL

--- ps/Check-FilesInOneDrive.ps1
+++ ps/Check-FilesInOneDrive.ps1
@@ -8,17 +8,18 @@ Import-Module ./Check-MatchFileInOtherLocation.psm1
 $NotInOneDrive = "NotInOneDrive.md"
 if (!(Test-Path $NotInOneDrive)) {
   New-item -ItemType File $NotInOneDrive
-}else{
+}
+else {
   clear-content $NotInOneDrive
 }
 
-  Write-host "Finding All Files in $source" -foregroundcolor green
-  $fromfiles = Get-ChildItem $source -recurse -name
-  Write-host "Found $($fromfiles.length) Files" -foregroundcolor yellow
+Write-host "Finding All Files in $source" -foregroundcolor green
+$fromfiles = Get-ChildItem $source -recurse -name
+Write-host "Found $($fromfiles.length) Files" -foregroundcolor yellow
 
-  Write-host "Finding All Files i$vault" -foregroundcolor green
-  $tofiles = Get-ChildItem $vault -recurse -name
-  Write-host "Found $($tofiles.length) Files" -foregroundcolor yellow
+Write-host "Finding All Files in $vault" -foregroundcolor green
+$tofiles = Get-ChildItem $vault -recurse -name
+Write-host "Found $($tofiles.length) Files" -foregroundcolor yellow
 
 Check-MatchFileInOtherLocation $fromfiles $tofiles $NotInOneDrive
 # Check-MatchFileInOtherLocation $vault $source $NotInGoogleDrive
--- ps/Check-MatchFileInOtherLocation.psm1
+++ ps/Check-MatchFileInOtherLocation.psm1
@@ -6,20 +6,22 @@ function Check-MatchFileInOtherLocation {
     $log
   )
   clear-content $log
-
+  $i = 1
   ForEach ($file in $from) {
     $found = $false
+    Write-Progress -Activity From $file -PercentComplete $(($i / $from.Length) * 100)
     foreach ($stored in $to) {
       if ($stored.contains($file)) {
         $found = $true
       }
     } 
     if ($found) {
-      write-host "Found $($file)" -ForegroundColor Green
+      write-host "Found $($file) No Action " -ForegroundColor Green
     }
     else {
       Out-File -InputObject $file -FilePath $log -Append
     }
+    $i++
   }
 }
 Export-ModuleMember -Function Check-MatchFileInOtherLocation
